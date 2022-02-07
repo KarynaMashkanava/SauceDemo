cca9e41a79541bd2ecd3512a77ea011fa9c9f3c3
KarinaMashkanova <karyna.mashkanava@gmail.com>
2022-02-08 01:01:51 +0300

    Revert "test case added"
    This reverts commit af0aaa7dd4fd5693ce9654a00372f5b459f82652.

--- src/test/java/Constants.java
+++
@@ -1,5 +0,0 @@
-public class Constants {
-    public static final String URL = "https://www.saucedemo.com/";
-    public static final String USERNAME = "standard_user";
-    public static final String PASSWORD = "secret_sauce";
-}
